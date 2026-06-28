2526ab0a5c3be8ead4d70293a00a09041b088df7
Chompy78 <stephb.johnc@gmail.com>
2026-06-28 16:36:35 +0800

    Add initial implementation of PACT engine

---
+++ js/engine.js
@@ -0,0 +1,11 @@
+// PACT Engine (DO NOT CHANGE LOGIC DURING EXTRACTION)
+
+export const PACT_ENGINE_VERSION = "v0.309";
+
+// Rules data (copy this next)
+export const DATA = {};
+
+// Core function (copy this next)
+export function compute(build) {
+  return {};
+}
